98b5023b3bbd3aa1c089555be2e8a141f0233add
akifkartal03 <akokartal@gmail.com>
2021-12-14 21:24:55 +0300

    add last

--- HW2/171044098.py
+++ HW2/171044098.py
@@ -1,8 +1,6 @@
-import math
-
-import matplotlib.pyplot as plt
+from nltk import collections
+from nltk.util import ngrams
 from turkishnlp import detector
-
 
 
 # I didn't want to bother with global variables therefore,
@@ -12,24 +10,32 @@ class HW2:
     def __init__(self):
         # write class variables here.
         self.__nlp = detector.TurkishNLP()
-        self.__wordList1 = []
-        self.__numberOfWord = 0
-        self.__sum2 = 0
-        self.__grp = []
-        self.__sem_sim = []
-        self.__firstLine = ""
+        self.__ngramTable = None
+        self.__ngramTableSize = 0
+        self.__wordCounts = None
+        self.__countTable = None
+        self.__GtTable = None
+        self.__n1 = None
+
         self.__testFile1 = None
         self.__testFile2 = None
         self.__testFile3 = None
         self.__testFile4 = None
-        self.__syll_in = None
-        self.__txt_in = None
 
     def get_results(self):
-        self.__testFile1 = open('corpus_out.txt', 'r', encoding='utf-8')
-        self.__testFile2 = open('corpus_syl.txt', 'w', encoding='utf-8')
-        obj = detector.TurkishNLP()
+        print("test")
+        self.__calculateNgrams(2)
+        self.__createCountTable()
+        f1 = open("outputs/countTable.txt", "w", encoding="utf8")
+        f1.write(str(self.__countTable))
+        self.__gtSmoothing()
+        f1 = open("outputs/gtTable.txt", "w", encoding="utf8")
+        f1.write(str(self.__GtTable))
 
+    def __separate_syllables(self):
+        self.__testFile1 = open('inputs/corpus_out.txt', 'r', encoding='utf-8')
+        self.__testFile2 = open('inputs/corpus_syl2.txt', 'w', encoding='utf-8')
+        obj = detector.TurkishNLP()
 
         line = self.__testFile1.read()
         arr = obj.syllabicate_sentence(line)
@@ -40,189 +46,68 @@ class HW2:
         self.__testFile2.close()
         self.__testFile1.close()
 
-    def __calculate_sem_sim_syl(self):
+    def __calculateNgrams(self, size):
+        name = "outputs/myGram" + str(size) + ".txt"
+        print(name)
+        f = open('inputs/corpus_syl.txt', 'r', encoding='utf-8')
+        of = open(name, 'w', encoding='utf-8')
+        corpusSyl = f.read()
+        corpusSyl = corpusSyl.split(' ')
+        self.__ngramTable = list(ngrams(corpusSyl, size))
 
-        acp = []
-        self.__sum2 = 0
-        for line in self.__wordList1:
-            words = line.split()
-            mainList = []
+        f1 = open("outputs/counters.txt", "w", encoding="utf8")
+        self.__wordCounts = collections.Counter(self.__ngramTable)
+        f1.write(str(dict(self.__wordCounts)))
+        self.__ngramTableSize = len(self.__ngramTable)
+        print("size: " + str(self.__ngramTableSize))
+        of.write(' '.join(corpusSyl))
+        f.close()
+        of.close()
+        f1.close()
 
-            for word in words[0:2]:
-                arr = self.__nlp.syllabicate_sentence(word)
-                listOfVectors = []
+    def __lowerText(self):
+        f = open('inputs/corpus_test.txt', 'r', encoding='utf-8')
+        of = open('inputs/corpus_test3.txt', 'w', encoding='utf-8')
+        corpusSyl = f.read()
+        corpusSyl = corpusSyl.lower()
+        of.write(corpusSyl)
+        f.close()
+        of.close()
 
-                for element in arr:
-                    for inner_element in element:
-                        alist = self.__searchFile(inner_element, self.__syll_in)
-                        if alist is not None:
-                            listOfVectors.append(alist)
-                if len(listOfVectors) > 0:
-                    mainList.append(listOfVectors)
-            self.__addAndWrite(mainList, line, words[2])
-            acp.append(float(words[2]))
-        self.__sem_sim.append(100 - (self.__sum2 / len(self.__wordList1)))
-        return 100 - (self.__sum2 / len(self.__wordList1))
-
-    def __calculate_sem_sim_normal(self):
-        self.__sum2 = 0
-        counter = 0
-        for line in self.__wordList1:
-            words = line.split()
-            mainList = []
-
-            for word in words[0:2]:
-                alist = self.__searchFile(word, self.__txt_in)
-                if alist is not None:
-                    mainList.append(alist)
-                else:
-                    print("ERRORRRR!!")
-                    print(word)
-            if len(mainList) > 1:
-                acc = float(words[2])
-                ecp = abs(self.__cosine_similarity(mainList[0], mainList[1]) - acc)
-                self.__sum2 += (ecp / acc) * 100
+    def __createCountTable(self):
+        self.__countTable = dict()
+        for i in self.__wordCounts:
+            if self.__wordCounts[i] in self.__countTable:
+                self.__countTable[self.__wordCounts[i]] += 1
             else:
-                counter = counter + 1
-        self.__sem_sim.append(100 - (self.__sum2 / (self.__numberOfWord - counter)))
-        return 100 - (self.__sum2 / (self.__numberOfWord - counter))
+                self.__countTable[self.__wordCounts[i]] = 1
 
-    def __calculate_syn_sim_normal(self):
-        self.__sum2 = 0
-        counter = 0
-        for line in self.__wordList1:
-            words = line.split()
-            mainList = []
+    def __gtSmoothing(self):
+        self.__GtTable = dict()
+        if 1 in self.__countTable:
+            self.__n1 = self.__countTable[1]
+        else:
+            self.__n1 = 1
+        for i in self.__wordCounts:
+            c = self.__wordCounts[i]
 
-            for word in words[0:4]:
-                alist = self.__searchFile(word, self.__txt_in)
-                if alist is not None:
-                    mainList.append(alist)
-                else:
-                    print("ERRORRRR!!")
-                    print(word)
-            if len(mainList) > 3:
-                acc = float(words[4])
-                sum1_2 = [x + y for x, y in zip(mainList[1], mainList[2])]
-                sub12_0 = [x - y for x, y in zip(sum1_2, mainList[0])]
-                ecp = abs(self.__cosine_similarity(mainList[3], sub12_0) - acc)
-                self.__sum2 += (ecp / acc) * 100
+            if (c + 1) not in self.__countTable:
+                nc1 = 1
             else:
-                counter = counter + 1
-        self.__sem_sim.append(100 - (self.__sum2 / (self.__numberOfWord - counter)))
-        return 100 - (self.__sum2 / (self.__numberOfWord - counter))
+                nc1 = self.__countTable[c + 1]
 
-    def __calculate_syn_sim_syl(self):
-        # self.__testFile.seek(0, 0)
-        acp = []
-        self.__sum2 = 0
-        for line in self.__wordList1:
-            words = line.split()
-            mainList = []
+            if c not in self.__countTable:
+                nc = 1
+            else:
+                nc = self.__countTable[c]
 
-            for word in words[0:4]:
-                arr = self.__nlp.syllabicate_sentence(word)
-                listOfVectors = []
+            res1 = (((c + 1) * nc1) / nc) - ((c * ((c + 1) * nc1)) / self.__n1)
+            res2 = 1 - (((c + 1) * nc1) / self.__n1)
 
-                for element in arr:
-                    for inner_element in element:
-                        alist = self.__searchFile(inner_element, self.__syll_in)
-                        if alist is not None:
-                            listOfVectors.append(alist)
-                if len(listOfVectors) > 0:
-                    mainList.append(listOfVectors)
-            self.__addAndWrite2(mainList, line, words[4])
-            acp.append(float(words[4]))
-        self.__sem_sim.append(100 - (self.__sum2 / len(self.__wordList1)))
-        return 100 - (self.__sum2 / len(self.__wordList1))
+            self.__GtTable[i] = res1 / res2
 
-    def __searchFile(self, syllable, name):
-        name.seek(0, 0)
-        for line in name:
-            # print(line.split()[0])
-            if line.split()[0] == syllable:
-                str2 = syllable + " "
-                vectorLine = line.replace(str2, '')
-                # print(vectorLine)
-                return [float(i) for i in vectorLine.split()]
-                # return vectorLine.split()
-
-    def __closeFiles(self):
-        self.__testFile1.close()
-        self.__testFile2.close()
-        self.__testFile3.close()
-        self.__testFile4.close()
-        self.__syll_in.close()
-        self.__txt_in.close()
-
-    def __addAndWrite(self, vectorList, sylb, guess):
-        if len(vectorList) > 1:
-            vectList1 = [sum(elements) for elements in zip(*vectorList[0])]
-            vectList2 = [sum(elements) for elements in zip(*vectorList[1])]
-            result = self.__cosine_similarity(vectList1, vectList2)
-            acc = float(guess)
-            self.__sum2 += (abs(result - acc) / acc) * 100
-
-        else:
-            print(sylb + ": Not FOUND!!!")
-
-    def __addAndWrite2(self, vectorList, sylb, guess):
-        if len(vectorList) > 3:
-            vectList0 = [sum(elements) for elements in zip(*vectorList[0])]
-            vectList1 = [sum(elements) for elements in zip(*vectorList[1])]
-            vectList2 = [sum(elements) for elements in zip(*vectorList[2])]
-            vectList3 = [sum(elements) for elements in zip(*vectorList[3])]
-
-            sum1_2 = [x + y for x, y in zip(vectList1, vectList2)]
-            sub12_0 = [x - y for x, y in zip(sum1_2, vectList0)]
-            result = self.__cosine_similarity(vectList3, sub12_0)
-            acc = float(guess)
-            self.__sum2 += (abs(result - acc) / acc) * 100
-
-        else:
-            print(sylb + ": Not FOUND!!!")
-
-    def __initFields(self):
-        self.__testFile1 = open('test_files/semantic_sim.txt', 'r', encoding='utf-8')
-        self.__testFile2 = open('test_files/semantic_anlgy.txt', 'r', encoding='utf-8')
-        self.__testFile3 = open('test_files/syntactic_sim.txt', 'r', encoding='utf-8')
-        self.__testFile4 = open('test_files/syntactic_anlgy.txt', 'r', encoding='utf-8')
-        self.__syll_in = open('test_files/heceVektors.txt', 'r', encoding='utf-8')
-        self.__txt_in = open('test_files/word2vec2.txt', 'r', encoding="utf-8", errors='ignore')
-
-    def __cosine_similarity(self, vect1, vect2):
-        xx, xy, yy = 0, 0, 0
-        for i in range(len(vect1)):
-            x = vect1[i]
-            y = vect2[i]
-            xx += x * x
-            yy += y * y
-            xy += x * y
-        return xy / math.sqrt(xx * yy)
-
-    def __q9(self, syllable):
-        self.__txt_in.seek(0, 0)
-        for line in self.__txt_in:
-            # print(line.split()[0])
-            if line.split()[0] == syllable:
-                str2 = syllable + " "
-                vectorLine = line.replace(str2, '')
-                # print(vectorLine)
-                return [float(i) for i in vectorLine.split()]
-
-    def __drawGraph(self, title):
-
-        left = [2, 3]
-        height = self.__grp
-        tick_label = ['word2vec', 'syllable based']
-        plt.bar(left, height, tick_label=tick_label,
-                width=0.1, color=['blue', 'green'])
-
-        plt.xlabel('Methods')
-        plt.ylabel('accuracy(%)')
-        plt.title(title)
-        plt.show()
+        print(self.__n1)
 
 
-hw1 = HW2()
-hw1.get_results()
+hw2 = HW2()
+hw2.get_results()
